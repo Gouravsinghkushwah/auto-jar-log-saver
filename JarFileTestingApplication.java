e647c909d842a9f83d98873f9701baa8562140a0
Gourav singh kushwah <71500489+Gouravsinghkushwah@users.noreply.github.com>
2024-09-20 14:51:17 +0530

    Update JarFileTestingApplication.java

--- JarFileTestingApplication.java
+++ JarFileTestingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
 public class JarFileTestingApplication {
-
+   // Start Class
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(JarFileTestingApplication.class, args);
  		 new ClassA().method();
